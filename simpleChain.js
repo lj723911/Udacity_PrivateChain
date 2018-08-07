2a726fab622ca19aca35c9f12453048d111d65ec
刘靖 <ljing110@126.com>
2018-08-07 10:15:42 +0800

    1、修改blockchain对象 2、height直接存在DB里 3、添加Test error的方法

--- simpleChain.js
+++ simpleChain.js
@@ -1,13 +1,11 @@
 /* ===== SHA256 with Crypto-js ===============================
 |  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
 |  =========================================================*/
-// let database = require('./levelSandbox')
-const SHA256 = require('crypto-js/sha256');
 
+const SHA256 = require('crypto-js/sha256');
 const level = require('level');
 const chainDB = './chaindata';
 const db = level(chainDB);
-
 
 /* ===== Block Class ==============================
 |  Class with a constructor for block 			   |
@@ -29,10 +27,35 @@ class Block{
 
 class Blockchain{
   constructor(){
-		// this.chain = the height of latest block
-    this.top = null;
-		this.db = chainDB;
-    this.addBlock(new Block("First block in the chain - Genesis block"));
+		this.database = chainDB;
+		let keyNum = 0;
+		db.createReadStream()
+		  .on('data', function (data) {
+				if (data.key != 'height') keyNum ++;
+		  })
+		  .on('error', function (err) {
+		    console.log('Unable to read data stream!', err)
+		  })
+		  .on('close', function () {
+				if (keyNum === 0){
+					console.log('no block exists')
+					// creat genesis block
+					let time = new Date().getTime().toString().slice(0,-3);
+					let newBlock = new Block("First block in the chain - Genesis block");
+					newBlock.height = 0;
+					newBlock.time = time;
+					newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+					db.put(newBlock.height, JSON.stringify(newBlock), function(err){
+						if (err) return console.log('Block ' + key + ' submission failed', err);
+					})
+					// set height in DB
+  				db.put('height', newBlock.height, function(err){
+						if (err) return console.log('Block ' + key + ' submission failed', err);
+					})
+				} else {
+					console.log(keyNum + ' [block] already exist!')
+				}
+		  })
   }
 
   // Add new block
@@ -40,33 +63,33 @@ class Blockchain{
     // UTC timestamp
     newBlock.time = new Date().getTime().toString().slice(0,-3);
 
-    if(this.top != null){
-			this.top += 1;
-			// Block height
-			newBlock.height = this.top;
-			    // previous block hash
-			this.getBlock(newBlock.height - 1).then(function(value){
-				newBlock.previousBlockHash = value.hash;
+		db.get('height', function(err, value){
+			newBlock.height = parseInt(value) + 1;
+			db.get(newBlock.height - 1, function(err,value){
+				let obj = JSON.parse(value)
+				// previous block hash
+				newBlock.previousBlockHash = obj.hash
 				// Block hash with SHA256 using newBlock and converting to a string
 				newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
 				// Adding block object to chain
 				db.put(newBlock.height, JSON.stringify(newBlock), function(err){
 					if (err) return console.log('Block ' + key + ' submission failed', err);
 				})
+				db.put('height', newBlock.height, function(err){
+					if (err) return console.log('Block ' + key + ' submission failed', err);
+				})
 			})
-    } else {
-			// Genesis block
-			newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-			db.put(newBlock.height, JSON.stringify(newBlock), function(err){
-				if (err) return console.log('Block ' + key + ' submission failed', err);
-			})
-			this.top = 0;
-		}
+		})
   }
 
   // Get block height
   getBlockHeight(){
-    return this.top;
+		return db.get('height').then(function(value){
+			console.log('current Block height is: ', value)
+			return value
+		}).catch(function(err){
+			console.log('Not found!', err);
+		})
   }
 
   // get block
@@ -96,47 +119,68 @@ class Blockchain{
 	      console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
 	      return  false
 	    }
+		}).catch(function(err){
+			console.log('Not found!', err);
 		})
   }
 
- // Validate blockchain
-  validateChain(){
+  // Validate blockchain
+	validateChain(){
 		let errorLog = [];
-
-    for (var i = this.top; i >= 0; i--) {
-			// validate block
-		  this.validateBlock(i).then(function(value){
-				if(!value) errorLog.push(i)
-			})
-
-      // compare blocks hash link
-			this.getBlock(i).then(function(block){
-				let previousHash = block.previousBlockHash;
-				let n = block.height - 1;
-				if( n >= 0 ) {
-					db.get(n, function(err, value){
-						let preBlock = JSON.parse(value)
-						let blockHash = preBlock.hash
-						if(blockHash !== previousHash) {
-							 errorLog.push(i);
-						}
-						if ( n == 0 ) {
-							if(errorLog.length>0){
-								console.log('Block errors = ' + errorLog.length);
-								console.log('Blocks: '+ errorLog);
-							}else {
-								console.log('No errors detected');
-							}
-						}
+		let _this = this;
+		this.getBlockHeight().then(function(value){
+			for (var i = 0; i <= value; i++) {
+				_this.getBlock(i).then(function(block){
+					// validate block
+					let h = block.height
+					_this.validateBlock(h).then(function(val){
+						if(!val) errorLog.push(h)
 					})
-				}
-      })
-    }
+
+					// compare blocks hash link
+          let hash = block.hash;
+					let n = block.height + 1;
+					if( n <= value ) {
+						db.get(n, function(err, val){
+							let nextBlock = JSON.parse(val)
+							let preHash = nextBlock.previousBlockHash
+							if(hash !== preHash) {
+								 errorLog.push(n-1);
+							}
+							if(n == value) {
+								if(errorLog.length>0){
+									console.log('\n-------Errors Detected!-------\n')
+									console.log('Block errors = ' + errorLog.length);
+									console.log('Blocks: '+ errorLog);
+								} else {
+									console.log('No errors detected');
+								}
+						  }
+						})
+					}
+				})
+			}
+		})
+	}
+
+	// this method is for text error
+	changeData(key){
+		db.get(key, function(err,value){
+				let obj = JSON.parse(value)
+				// previous block hash
+				obj.time = new Date().getTime().toString().slice(0,-3);
+				// Block hash with SHA256 using newBlock and converting to a string
+				obj.hash = SHA256(JSON.stringify(obj)).toString();
+				// Adding block object to chain
+				db.put(key, JSON.stringify(obj), function(err){
+					if (err) return console.log('Block ' + key + ' submission failed', err);
+				})
+			})
 	}
 }
 
 let blockchain = new Blockchain();
-blockchain.addBlock(new Block());
-blockchain.addBlock(new Block());
-blockchain.addBlock(new Block());
-blockchain.addBlock(new Block());
+// blockchain.addBlock(new Block());
+// blockchain.addBlock(new Block());
+// blockchain.addBlock(new Block());
+// blockchain.addBlock(new Block());
